f90368d7f841a0041c83190977c688f35fe009e2
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 20:10:27 -0300

    Adicionado command delete para rotas

---
+++ RotaViagem.Application/Commands/Rotas/RotaDeleteCommand.cs
@@ -0,0 +1,16 @@
+﻿using RotaViagem.Application.Validations;
+
+namespace RotaViagem.Application.Commands.Rotas;
+
+public class RotaDeleteCommand: Notificavel, ICommands
+{
+    public int Id { get; set; }
+
+    public void Validar()
+    {
+        if (Id <= 0)
+        {
+            AddErro("O Código da rota é obrigatória!");
+        }
+    }
+}
