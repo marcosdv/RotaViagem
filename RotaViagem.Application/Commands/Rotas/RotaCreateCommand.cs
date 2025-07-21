91829c049816cbd01d043fdcebc067a38405e4d8
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 19:59:50 -0300

    Adicionado command create para rotas

---
+++ RotaViagem.Application/Commands/Rotas/RotaCreateCommand.cs
@@ -0,0 +1,27 @@
+﻿using RotaViagem.Application.Validations;
+
+namespace RotaViagem.Application.Commands.Rotas
+{
+    public class RotaCreateCommand : Notificavel, ICommands
+    {
+        public string Origem { get; set; } = string.Empty;
+        public string Destino { get; set; } = string.Empty;
+        public double Preco { get; set; }
+
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(Origem))
+            {
+                AddErro("A Origem da rota é obrigatória!");
+            }
+            if (string.IsNullOrEmpty(Destino))
+            {
+                AddErro("O Destino da rota é obrigatório!");
+            }
+            if (Preco <= 0)
+            {
+                AddErro("O Preço da rota deve ser maior que zero!");
+            }
+        }
+    }
+}
