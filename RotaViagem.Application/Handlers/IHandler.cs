129fcc9187e2cfcbeaacd3ba0890ce66fafb30d7
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 20:34:13 -0300

    Criado interface dos handlers

---
+++ RotaViagem.Application/Handlers/IHandler.cs
@@ -0,0 +1,8 @@
+﻿using RotaViagem.Application.Commands;
+
+namespace RotaViagem.Application.Handlers;
+
+public interface IHandler<T> where T : ICommands
+{
+    Task<ICommandResult> Execute(T command);
+}
