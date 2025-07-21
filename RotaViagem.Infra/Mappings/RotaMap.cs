8e03455bb2ec3d59a3a97fc3c4288097001f37ab
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 20:49:08 -0300

    Adicionado mapeamento da rota

---
+++ RotaViagem.Infra/Mappings/RotaMap.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RotaViagem.Domain.Entities;
+
+namespace RotaViagem.Infra.Mappings;
+
+public class RotaMap : IEntityTypeConfiguration<Rota>
+{
+    public void Configure(EntityTypeBuilder<Rota> builder)
+    {
+        builder.ToTable("TbRota");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("RotId")
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Origem)
+            .HasColumnName("RotOrigem")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(x => x.Destino)
+            .HasColumnName("RotDestino")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(x => x.Preco)
+            .HasColumnName("RotPreco")
+            .IsRequired();
+    }
+}
