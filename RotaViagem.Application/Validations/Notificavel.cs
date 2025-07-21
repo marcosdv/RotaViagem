a18bb2b6e9fcd94f1eefb7c1089c505a5f33517d
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 19:55:31 -0300

    Adicionada classe para validacao

---
+++ RotaViagem.Application/Validations/Notificavel.cs
@@ -0,0 +1,22 @@
+﻿namespace RotaViagem.Application.Validations;
+
+public abstract class Notificavel
+{
+    private readonly List<string> _listaErros;
+
+    protected Notificavel()
+    {
+        _listaErros = new List<string>();
+    }
+
+    public void AddErro(string texto)
+    {
+        _listaErros.Add(texto);
+    }
+
+    public IReadOnlyCollection<string> Erros => _listaErros;
+
+    public bool ExisteErro => _listaErros.Any();
+
+    public bool SemErro => !ExisteErro;
+}
