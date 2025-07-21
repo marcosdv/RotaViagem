f903551e74ba583420c1ed02a1f5122a151ea065
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 20:10:12 -0300

    Adicionado command update para rotas

---
+++ RotaViagem.Application/Commands/Rotas/RotaUpdateCommand.cs
@@ -0,0 +1,31 @@
+﻿using RotaViagem.Application.Validations;
+
+namespace RotaViagem.Application.Commands.Rotas;
+
+public class RotaUpdateCommand: Notificavel, ICommands
+{
+    public int Id { get; set; }
+    public string Origem { get; set; } = string.Empty;
+    public string Destino { get; set; } = string.Empty;
+    public double Preco { get; set; }
+
+    public void Validar()
+    {
+        if (Id <= 0)
+        {
+            AddErro("O Código da rota é obrigatória!");
+        }
+        if (string.IsNullOrEmpty(Origem))
+        {
+            AddErro("A Origem da rota é obrigatória!");
+        }
+        if (string.IsNullOrEmpty(Destino))
+        {
+            AddErro("O Destino da rota é obrigatório!");
+        }
+        if (Preco <= 0)
+        {
+            AddErro("O Preço da rota deve ser maior que zero!");
+        }
+    }
+}
