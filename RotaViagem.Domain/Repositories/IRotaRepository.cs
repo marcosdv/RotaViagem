2d23ca2f093dddb13baa20bf822ab0a2203e0ecf
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 19:41:46 -0300

    Adicionando a interface do repository Rota

---
+++ RotaViagem.Domain/Repositories/IRotaRepository.cs
@@ -0,0 +1,13 @@
+﻿using RotaViagem.Domain.Entities;
+
+namespace RotaViagem.Domain.Repositories;
+
+public interface IRotaRepository
+{
+    Task<Rota> GetByIdAsync(Guid id);
+    Task<IEnumerable<Rota>> GetAllAsync();
+
+    Task AddAsync(Rota route);
+    Task UpdateAsync(Rota route);
+    Task DeleteAsync(Rota route);
+}
