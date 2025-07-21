3fbdc5e7448c2e1af527914bc7a31abfc8723031
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 20:10:45 -0300

    Adicionado command result

---
+++ RotaViagem.Application/Commands/Results/CommandResult.cs
@@ -0,0 +1,15 @@
+﻿namespace RotaViagem.Application.Commands.Results;
+
+public class CommandResult : ICommandResult
+{
+    public CommandResult(bool sucesso, string mensagem, object dados)
+    {
+        Sucesso = sucesso;
+        Mensagem = mensagem;
+        Dados = dados;
+    }
+
+    public bool Sucesso { get; set; }
+    public string Mensagem { get; set; }
+    public object Dados { get; set; }
+}
