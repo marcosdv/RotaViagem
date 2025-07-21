040ad350d754405f1d28d82886ea1111c20d2b13
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 20:34:52 -0300

    Criado handler de update da rota

---
+++ RotaViagem.Application/Handlers/Rotas/RotaUpdateHandler.cs
@@ -0,0 +1,38 @@
+﻿using RotaViagem.Application.Commands;
+using RotaViagem.Application.Commands.Results;
+using RotaViagem.Application.Commands.Rotas;
+using RotaViagem.Domain.Repositories;
+
+namespace RotaViagem.Application.Handlers.Rotas;
+
+public class RotaUpdateHandler : IHandler<RotaUpdateCommand>
+{
+    private readonly IRotaRepository _rotaRepository;
+
+    public RotaUpdateHandler(IRotaRepository rotaRepository)
+    {
+        _rotaRepository = rotaRepository;
+    }
+
+    public async Task<ICommandResult> Execute(RotaUpdateCommand command)
+    {
+        command.Validar();
+        if (command.ExisteErro)
+        {
+            return new CommandResult(false, "Dados da rota incorretos", command.Erros);
+        }
+
+        var rota = await _rotaRepository.GetByIdAsync(command.Id);
+
+        if (rota == null)
+        {
+            return new CommandResult(false, "Rota não encontrada", command.Erros);
+        }
+
+        rota.Update(command.Origem, command.Destino, command.Preco);
+
+        await _rotaRepository.UpdateAsync(rota);
+
+        return new CommandResult(true, "Rota atualizada", rota);
+    }
+}
