88d6fd5d33d6b06270fe34ebda49112601a7f987
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 20:49:44 -0300

    Adicionado context do EF

---
+++ RotaViagem.Infra/Contexts/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RotaViagem.Domain.Entities;
+using RotaViagem.Infra.Mappings;
+
+namespace RotaViagem.Infra.Contexts;
+
+public class DataContext : DbContext
+{
+    public DataContext() { }
+
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options) { }
+
+    public DbSet<Rota> Rotas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new RotaMap());
+    }
+}
