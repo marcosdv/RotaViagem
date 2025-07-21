196f9af573402549377465e4e7ee6c61f00c6ad8
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 19:39:44 -0300

    Adicionando a entidade Rota

---
+++ RotaViagem.Domain/Entities/Rota.cs
@@ -0,0 +1,9 @@
+﻿namespace RotaViagem.Domain.Entities;
+
+public class Rota
+{
+    public int Id { get; set; }
+    public string Origem { get; set; }
+    public string Destino { get; set; }
+    public double Preco { get; set; }
+}
