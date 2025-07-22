26b25dbb01035bbc2c140c4b8f7b646a25e3c3ae
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 23:25:32 -0300

    Criado teste unitario para RotaGetAllHandler

---
+++ RotaViagem.Tests/Application/Handlers/Rotas/RotaGetAllHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using RotaViagem.Application.Handlers.Rotas;
+using RotaViagem.Application.Queries.Results;
+using RotaViagem.Application.Queries.Rotas;
+using RotaViagem.Domain.Repositories;
+using RotaViagem.Tests.Application.Repositories;
+
+namespace RotaViagem.Tests.Application.Handlers.Rotas;
+
+[TestClass]
+public class RotaGetAllHandlerTests
+{
+    private readonly IRotaRepository _repository;
+    private readonly RotaGetAllHandler _handler;
+
+    public RotaGetAllHandlerTests()
+    {
+        _repository = new MockRotaRepository();
+        _handler = new RotaGetAllHandler(_repository);
+    }
+
+    [TestMethod]
+    public async Task DadoUmComandoValidoDeveRetornarSucessoTrue()
+    {
+        var command = new RotaGetAllQuery();
+
+        var result = await _handler.Execute(command);
+
+        Assert.IsTrue(((QueriesResult)result).Sucesso);
+    }
+}
