e2e20fd5d3d1e73e3b78d4f6d57aba9bca19b521
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 22:11:17 -0300

    Iniciado query para get melhor rota

---
+++ RotaViagem.Application/Handlers/Rotas/RotaGetMelhorRotaHandler.cs
@@ -0,0 +1,34 @@
+﻿using RotaViagem.Application.Commands;
+using RotaViagem.Application.Commands.Results;
+using RotaViagem.Application.Queries.Results;
+using RotaViagem.Application.Queries.Rotas;
+using RotaViagem.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotaViagem.Application.Handlers.Rotas;
+
+public class RotaGetMelhorRotaHandler : IQueryHandler<RotaGetMelhorRotaQuery>
+{
+    private readonly IRotaRepository _rotaRepository;
+
+    public RotaGetMelhorRotaHandler(IRotaRepository rotaRepository)
+    {
+        _rotaRepository = rotaRepository;
+    }
+
+    public async Task<ICommandResult> Execute(RotaGetMelhorRotaQuery command)
+    {
+        command.Validar();
+        if (command.ExisteErro)
+        {
+            return new CommandResult(false, "Dados de busca incorretos", command.Erros);
+        }
+
+        var rotas = await _rotaRepository.GetAllAsync();
+        return new QueriesResult(true, rotas);
+    }
+}
---
+++ RotaViagem.Application/Queries/Rotas/RotaGetMelhorRotaQuery.cs
@@ -0,0 +1,21 @@
+﻿using RotaViagem.Application.Validations;
+
+namespace RotaViagem.Application.Queries.Rotas;
+
+public class RotaGetMelhorRotaQuery : Notificavel, IQueries
+{
+    public string Origem { get; set; } = string.Empty;
+    public string Destino { get; set; } = string.Empty;
+
+    public void Validar()
+    {
+        if (string.IsNullOrWhiteSpace(Origem))
+        {
+            AddErro("A Origem da rota é obrigatória!");
+        }
+        if (string.IsNullOrWhiteSpace(Destino))
+        {
+            AddErro("O Destino da rota é obrigatório!");
+        }
+    }
+}
