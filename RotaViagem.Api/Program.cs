d3ccc87136e91ae58b7d7d3d0470e8436784693a
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 21:30:43 -0300

    Adicionada configuracao do EF

--- RotaViagem.Api/Program.cs
+++ RotaViagem.Api/Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using RotaViagem.Application.Handlers.Rotas;
+using RotaViagem.Domain.Repositories;
+using RotaViagem.Infra.Contexts;
+using RotaViagem.Infra.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//DI - Injecao de Dependencias
+builder.Services.AddDbContext<DataContext>(x => 
+    x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+//DI - Injecao de Dependencias
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
