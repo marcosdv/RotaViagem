b2ae9ccc64ad61d803a712a0b4bef95bce60e636
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 21:26:35 -0300

    Criado migrations inicial

---
+++ RotaViagem.Infra/Migrations/20250722000547_CriacaoInicial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RotaViagem.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TbRota",
+                columns: table => new
+                {
+                    RotId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RotOrigem = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    RotDestino = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    RotPreco = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TbRota", x => x.RotId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TbRota");
+        }
+    }
+}
