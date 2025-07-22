21f4f6c471d907ea4d3137247078c0801e50c4d6
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 21:26:50 -0300

    Adicionado Script SQL do banco de dados

---
+++ RotaViagem.Infra/script.sql
@@ -0,0 +1,25 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [TbRota] (
+    [RotId] uniqueidentifier NOT NULL,
+    [RotOrigem] nvarchar(50) NOT NULL,
+    [RotDestino] nvarchar(50) NOT NULL,
+    [RotPreco] float NOT NULL,
+    CONSTRAINT [PK_TbRota] PRIMARY KEY ([RotId])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250722000547_CriacaoInicial', N'9.0.7');
+
+COMMIT;
+GO
+
