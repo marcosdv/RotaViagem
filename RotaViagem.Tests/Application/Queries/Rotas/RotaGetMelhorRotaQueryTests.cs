dc045e3d53fb2b81e2012d9b13ace95066409df0
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 23:02:29 -0300

    Adicionado teste unitario do RotaGetMelhorRotaQueryTests

---
+++ RotaViagem.Tests/Application/Queries/Rotas/RotaGetMelhorRotaQueryTests.cs
@@ -0,0 +1,49 @@
+﻿using RotaViagem.Application.Queries.Rotas;
+
+namespace RotaViagem.Tests.Application.Queries.Rotas;
+
+[TestClass]
+public class RotaGetMelhorRotaQueryTests
+{
+    [TestMethod]
+    public void DadoUmaQueryValidoDeveRetornarSemErro()
+    {
+        var query = new RotaGetMelhorRotaQuery
+        {
+            Origem = "Origem",
+            Destino = "Destino"
+        };
+
+        query.Validar();
+
+        Assert.IsTrue(query.SemErro);
+    }
+
+    [TestMethod]
+    public void DadoUmaQuerySemOrigemDeveRetornarExisteErro()
+    {
+        var query = new RotaGetMelhorRotaQuery
+        {
+            Destino = "Destino"
+        };
+
+        query.Validar();
+
+        Assert.IsTrue(query.ExisteErro);
+        Assert.IsTrue(query.Erros.Any());
+    }
+
+    [TestMethod]
+    public void DadoUmaQuerySemDestinoDeveRetornarExisteErro()
+    {
+        var query = new RotaGetMelhorRotaQuery
+        {
+            Origem = "Origem"
+        };
+
+        query.Validar();
+
+        Assert.IsTrue(query.ExisteErro);
+        Assert.IsTrue(query.Erros.Any());
+    }
+}
---
+++ RotaViagem.Tests/Application/Repositories/MockRotaRepository.cs
@@ -0,0 +1,38 @@
+﻿using RotaViagem.Domain.Entities;
+using RotaViagem.Domain.Repositories;
+
+namespace RotaViagem.Tests.Application.Repositories;
+
+public class MockRotaRepository : IRotaRepository
+{
+    private readonly IList<Rota> _rotas;
+
+    public MockRotaRepository()
+    {
+        _rotas = [
+            new Rota(Guid.NewGuid(), "GRU", "BRC", 10),
+            new Rota(Guid.NewGuid(), "BRC", "SCL", 5),
+            new Rota(Guid.NewGuid(), "GRU", "CDG", 75),
+            new Rota(Guid.NewGuid(), "GRU", "SCL", 20),
+            new Rota(Guid.NewGuid(), "GRU", "ORL", 56),
+            new Rota(Guid.NewGuid(), "ORL", "CDG", 5),
+            new Rota(Guid.NewGuid(), "SCL", "ORL", 20),
+        ];
+    }
+
+    public Task<Rota?> GetByIdAsync(Guid id)
+    {
+        return Task.FromResult(_rotas.FirstOrDefault(r => r.Id == id));
+    }
+
+    public Task<IEnumerable<Rota>> GetAllAsync()
+    {
+        return Task.FromResult<IEnumerable<Rota>>(_rotas);
+    }
+
+    public async Task AddAsync(Rota rota) { }
+
+    public async Task UpdateAsync(Rota rota) { }
+
+    public async Task DeleteAsync(Rota rota) { }
+}
