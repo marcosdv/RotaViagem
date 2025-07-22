c2679248d936266dc0cfa352df8d7f479e812ea9
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 21:26:13 -0300

    Adicionado handler do delete rota

--- RotaViagem.Application/Handlers/Rotas/RotaDeleteHandler.cs
+++ RotaViagem.Application/Handlers/Rotas/RotaDeleteHandler.cs
@@ -1,11 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RotaViagem.Application.Commands;
+using RotaViagem.Application.Commands.Results;
+using RotaViagem.Application.Commands.Rotas;
+using RotaViagem.Domain.Repositories;
 
 namespace RotaViagem.Application.Handlers.Rotas;
 
 public class RotaDeleteHandler
 {
+    private readonly IRotaRepository _rotaRepository;
+
+    public RotaDeleteHandler(IRotaRepository rotaRepository)
+    {
+        _rotaRepository = rotaRepository;
+    }
+
+    public async Task<ICommandResult> Execute(RotaDeleteCommand command)
+    {
+        command.Validar();
+        if (command.ExisteErro)
+        {
+            return new CommandResult(false, "Dados da rota incorretos", command.Erros);
+        }
+
+        var rota = await _rotaRepository.GetByIdAsync(command.Id);
+
+        if (rota == null)
+        {
+            return new CommandResult(false, "Rota não encontrada", command.Erros);
+        }
+
+        await _rotaRepository.DeleteAsync(rota);
+
+        return new CommandResult(true, "Rota excluída", rota);
+    }
 }
