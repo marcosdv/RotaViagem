9827b1bfd029b3f915a80e67080f1f59a922be48
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 22:04:44 -0300

    Adicionado QueryResults para padrao de retorno

---
+++ RotaViagem.Application/Queries/Results/QueriesResult.cs
@@ -0,0 +1,15 @@
+﻿using RotaViagem.Application.Commands;
+
+namespace RotaViagem.Application.Queries.Results;
+
+public class QueriesResult : ICommandResult
+{
+    public QueriesResult(bool sucesso, object? dados)
+    {
+        Sucesso = sucesso;
+        Dados = dados;
+    }
+
+    public bool Sucesso { get; set; }
+    public object? Dados { get; set; }
+}
