29ae645174c0c79a58ac1e8ee7c883e76adeb626
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 22:18:43 -0300

    Adicionado response para melhor rota

---
+++ RotaViagem.Application/Queries/Response/MelhorRotaResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace RotaViagem.Application.Queries.Response;
+
+public class MelhorRotaResponse
+{
+    public string Rota { get; set; } = string.Empty;
+    public double PrecoTotal { get; set; }
+}
