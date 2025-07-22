1f742683ed996a5d588bc86b0d7469158889db6a
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 22:41:31 -0300

    Adicionado teste unitario para RotaCreateCommand

---
+++ RotaViagem.Tests/Application/Commands/Rotas/RotaCreateCommandTests.cs
@@ -0,0 +1,69 @@
+﻿using Moq;
+using RotaViagem.Application.Commands.Rotas;
+
+namespace RotaViagem.Tests.Application.Commands.Rotas
+{
+    [TestClass]
+    public class RotaCreateCommandTests
+    {
+        [TestMethod]
+        public void DadoUmComandoValidoDeveRetornarSemErro()
+        {
+            var command = new RotaCreateCommand
+            {
+                Origem = "Origem",
+                Destino = "Destino",
+                Preco = 10
+            };
+
+            command.Validar();
+
+            Assert.IsTrue(command.SemErro);
+        }
+
+        [TestMethod]
+        public void DadoUmComandoSemOrigemDeveRetornarExisteErro()
+        {
+            var command = new RotaCreateCommand
+            {
+                Destino = "Destino",
+                Preco = 10
+            };
+
+            command.Validar();
+
+            Assert.IsTrue(command.ExisteErro);
+            Assert.IsTrue(command.Erros.Any());
+        }
+
+        [TestMethod]
+        public void DadoUmComandoSemDestinoDeveRetornarExisteErro()
+        {
+            var command = new RotaCreateCommand
+            {
+                Origem = "Origem",
+                Preco = 10
+            };
+
+            command.Validar();
+
+            Assert.IsTrue(command.ExisteErro);
+            Assert.IsTrue(command.Erros.Any());
+        }
+
+        [TestMethod]
+        public void DadoUmComandoSemPrecoDeveRetornarExisteErro()
+        {
+            var command = new RotaCreateCommand
+            {
+                Origem = "Origem",
+                Destino = "Destino"
+            };
+
+            command.Validar();
+
+            Assert.IsTrue(command.ExisteErro);
+            Assert.IsTrue(command.Erros.Any());
+        }
+    }
+}
