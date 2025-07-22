0a054236726652aeb3166fad6723ba39bb1c9b76
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 22:49:29 -0300

    Adicionado teste unitario para RotaUpdateCommand

---
+++ RotaViagem.Tests/Application/Commands/Rotas/RotaUpdateCommandTests.cs
@@ -0,0 +1,87 @@
+﻿using RotaViagem.Application.Commands.Rotas;
+
+namespace RotaViagem.Tests.Application.Commands.Rotas;
+
+[TestClass]
+public class RotaUpdateCommandTests
+{
+    [TestMethod]
+    public void DadoUmComandoValidoDeveRetornarSemErro()
+    {
+        var command = new RotaUpdateCommand
+        {
+            Id = Guid.NewGuid(),
+            Origem = "Origem",
+            Destino = "Destino",
+            Preco = 10
+        };
+
+        command.Validar();
+
+        Assert.IsTrue(command.SemErro);
+    }
+
+    [TestMethod]
+    public void DadoUmComandoSemIdDeveRetornarExisteErro()
+    {
+        var command = new RotaUpdateCommand
+        {
+            Origem = "Origem",
+            Destino = "Destino",
+            Preco = 10
+        };
+
+        command.Validar();
+
+        Assert.IsTrue(command.ExisteErro);
+        Assert.IsTrue(command.Erros.Any());
+    }
+
+    [TestMethod]
+    public void DadoUmComandoSemOrigemDeveRetornarExisteErro()
+    {
+        var command = new RotaUpdateCommand
+        {
+            Id = Guid.NewGuid(),
+            Destino = "Destino",
+            Preco = 10
+        };
+
+        command.Validar();
+
+        Assert.IsTrue(command.ExisteErro);
+        Assert.IsTrue(command.Erros.Any());
+    }
+
+    [TestMethod]
+    public void DadoUmComandoSemDestinoDeveRetornarExisteErro()
+    {
+        var command = new RotaUpdateCommand
+        {
+            Id = Guid.NewGuid(),
+            Origem = "Origem",
+            Preco = 10
+        };
+
+        command.Validar();
+
+        Assert.IsTrue(command.ExisteErro);
+        Assert.IsTrue(command.Erros.Any());
+    }
+
+    [TestMethod]
+    public void DadoUmComandoSemPrecoDeveRetornarExisteErro()
+    {
+        var command = new RotaUpdateCommand
+        {
+            Id = Guid.NewGuid(),
+            Origem = "Origem",
+            Destino = "Destino"
+        };
+
+        command.Validar();
+
+        Assert.IsTrue(command.ExisteErro);
+        Assert.IsTrue(command.Erros.Any());
+    }
+}
