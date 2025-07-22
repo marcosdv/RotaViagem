9447037b98fbac57d52e8fc4cf128e6adb1ae7f2
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 22:49:46 -0300

    Adicionado teste unitario para RotaDeleteCommand

---
+++ RotaViagem.Tests/Application/Commands/Rotas/RotaDeleteCommandTests.cs
@@ -0,0 +1,31 @@
+﻿using RotaViagem.Application.Commands.Rotas;
+
+namespace RotaViagem.Tests.Application.Commands.Rotas;
+
+[TestClass]
+public class RotaDeleteCommandTests
+{
+    [TestMethod]
+    public void DadoUmComandoValidoDeveRetornarSemErro()
+    {
+        var command = new RotaDeleteCommand
+        {
+            Id = Guid.NewGuid()
+        };
+
+        command.Validar();
+
+        Assert.IsTrue(command.SemErro);
+    }
+
+    [TestMethod]
+    public void DadoUmComandoSemIdDeveRetornarExisteErro()
+    {
+        var command = new RotaDeleteCommand();
+
+        command.Validar();
+
+        Assert.IsTrue(command.ExisteErro);
+        Assert.IsTrue(command.Erros.Any());
+    }
+}
