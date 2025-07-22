564adf0262462d11c6a4eb2a02434791f5823a17
marcosvendramini <marcosvendramini@hotmail.com>
2025-07-21 23:00:30 -0300

    Adicionado teste unitario do RotaGetByIdQuery

---
+++ RotaViagem.Tests/Application/Queries/Rotas/RotaGetByIdQueryTests.cs
@@ -0,0 +1,31 @@
+﻿using RotaViagem.Application.Queries.Rotas;
+
+namespace RotaViagem.Tests.Application.Queries.Rotas;
+
+[TestClass]
+public class RotaGetByIdQueryTests
+{
+    [TestMethod]
+    public void DadoUmaQueryValidoDeveRetornarSemErro()
+    {
+        var query = new RotaGetByIdQuery
+        {
+            Id = Guid.NewGuid()
+        };
+
+        query.Validar();
+
+        Assert.IsTrue(query.SemErro);
+    }
+
+    [TestMethod]
+    public void DadoUmaQuerySemIdDeveRetornarExisteErro()
+    {
+        var query = new RotaGetByIdQuery();
+
+        query.Validar();
+
+        Assert.IsTrue(query.ExisteErro);
+        Assert.IsTrue(query.Erros.Any());
+    }
+}
